133e9532b65947a0af51bb415ac6211f7e1e9da3
LUAN BARBOSA MENEGUELLI <luan.meneguelli@alunos.sc.senac.br>
2022-06-23 20:59:52 -0300

    Exercicio de qualidade de software

---
+++ Sistema-de-Login/Senac.Tests/UsuarioTests.cs
@@ -0,0 +1,66 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Senac.Tests
+{
+    [TestClass]
+    public class UsuarioTests
+    {
+        [TestMethod]        
+        public void Login_EmailSenhaCorretos()
+        {            
+            Usuario usuario = new Usuario();            
+            var resultado = usuario.Login("marcelo.petri@prof.sc.senac.br", "123456");
+
+            Assert.AreEqual("Usuário logado", resultado);        
+        }
+
+        [TestMethod]       
+        public void Login_EmailIncorreto()
+        {            
+            Usuario usuario = new Usuario();            
+            var resultado = usuario.Login("marcelo.petri@senac.br", "123456");  
+
+            Assert.AreEqual("E-mail ou senha incorretos", resultado);        
+        }
+
+        [TestMethod]        
+        public void Login_SenhaIncorreto()
+        {            
+            Usuario usuario = new Usuario();            
+            var resultado = usuario.Login("marcelo.petri@prof.sc.senac.br", "123");     
+
+            Assert.AreEqual("E-mail ou senha incorretos", resultado);        
+        }
+
+        [TestMethod]        
+        public void Login_SenhaVazia()
+        {            
+            Usuario usuario = new Usuario();           
+            var resultado = usuario.Login("marcelo.petri@prof.sc.senac.br", "");  
+
+            Assert.AreEqual("Preencha todos os dados!", resultado);        
+        }
+
+        [TestMethod]        
+        public void Login_EmailVazio()
+        {            
+            Usuario usuario = new Usuario();            
+            var resultado = usuario.Login("", "123456");            
+            Assert.AreEqual("Preencha todos os dados!", resultado);        
+        }
+
+        [DataTestMethod]        
+        [DataRow("")]        
+        [DataRow(null)]        
+        [DataRow("")]        
+        public void VerificarNome_RetornoFalso(string value)
+        {            
+            Usuario usuario = new Usuario();            
+            var resultado = usuario.VerificarNome(value);     
+
+            Assert.IsFalse(resultado);        
+        }
+
+        
+    }
+}
